f68af76abc407f6d2b4ae7e692d084221f793595
ztnark <grouponloaner33@gm10599.group.on>
2013-11-04 09:04:25 -0600

    tweaks

--- seed.rb
+++ seed.rb
@@ -1,3 +1,25 @@
 20.times do |x|
-  Truck.create(name: "Test #{x}", latitude: "41.8#{rand(5..9)}#{rand(1..9)}400", longitude: "-87.6#{rand(2..5)}#{rand(1..9)}3044", url: "http://www.test.com")
+  Truck.create(name: @names.shuffle.pop, latitude: "41.8#{rand(5..9)}#{rand(1..9)}400", longitude: "-87.6#{rand(2..5)}#{rand(1..9)}3044", url: "http://www.test.com")
 end
+
+@names = ["Jerk 312",
+        "Fat Shallot",
+        "Toasty Cheese",
+        "Yum Dum Truck",
+        "Babycakes Gourment",
+        "The Roost",
+        "Beavers Donuts",
+        "Caponies Express",
+        "Schnitzel King",
+        "Two Italians",
+        "Duck n' Roll",
+        "ChiTown Tamale",
+        "La Adelita",
+        "Samich Box",
+        "Chubby Wieners",
+        "Curried",
+        "Ja' Grill",
+        "Haute Sausage",
+        "Taquero Fusion",
+        "Steakwich",
+        "Wagya Wagon"]
